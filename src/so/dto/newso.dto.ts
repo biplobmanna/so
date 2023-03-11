55622c0bb1a21832f133219b69bb2bbe1d84d229
biplobmanna <biplobmanna@outlook.com>
2023-03-11 21:23:47 +0530

    fix: tabs -> spaces

--- src/so/dto/newso.dto.ts
+++ src/so/dto/newso.dto.ts
@@ -1,11 +1,11 @@
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class NewSoDto {
-	@IsString()
-	@IsNotEmpty()
-	content: string;
+  @IsString()
+  @IsNotEmpty()
+  content: string;
 
-	@IsString()
-	@IsOptional()
-	tag?: string;
+  @IsString()
+  @IsOptional()
+  tag?: string;
 }
