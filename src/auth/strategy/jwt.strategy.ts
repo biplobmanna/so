1115f7c671a2e77a6f837a73781c06ebeeefefac
biplobmanna <biplobmanna@outlook.com>
2023-03-11 21:09:24 +0530

    update: pretty print error

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,24 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PrismaService } from '../../prisma/prisma.service';
+import { JwtDto } from '../dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-	constructor(config: ConfigService, private prisma: PrismaService) {
-		super({
-			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			secretOrKey: config.get('JWT_SECRET'),
-		});
-	}
-	async validate(payload: any) {
-		const user = await this.prisma.user.findUnique({
-			where: {
-				id: payload.sub,
-			},
-		});
-		delete user.hash;
-		return user;
-	}
+  constructor(config: ConfigService, private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+  async validate(payload: JwtDto) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id: payload.sub,
+        },
+        select: {
+          id: true,
+          username: true,
+          email: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      throw new UnauthorizedException('⛔ Unauthorized Access!');
+    }
+  }
 }
