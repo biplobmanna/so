4b7e8be54663cc48f74e85a95daca86ffa991a0d
biplobmanna <biplobmanna@outlook.com>
2023-03-11 21:23:18 +0530

    update: register so gateway service

--- src/so/so.module.ts
+++ src/so/so.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SoController } from './so.controller';
 import { SoService } from './so.service';
+import { SoGateway } from './so.gateway';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-	controllers: [SoController],
-	providers: [SoService],
+  imports: [JwtModule.register({})],
+  controllers: [SoController],
+  providers: [SoService, SoGateway],
 })
 export class SoModule {}
