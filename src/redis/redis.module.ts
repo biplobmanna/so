78fdd9c6b7d44c80293c7769311f44fced4be747
biplobmanna <biplobmanna@outlook.com>
2023-03-11 22:43:24 +0530

    add: redis module
    caching with redis on docker container
    
    `cache-manager-redis-store` package v3.0.0 is breaking for regular use
    so, registerAsync() is used instead of register()
    
    REDIS_PORT & REDIS_HOST is fetched from .env
    
    module activated globally, so no @Inject() syntax to be used
    in Controllers
    
    used in User, So modules

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,31 @@
+import { CacheModule, CacheStore, Module } from '@nestjs/common';
+import { RedisService } from './redis.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
+
+// @Global()
+@Module({
+  imports: [
+    // workaround for redis ^4.0.0
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      // inconsistency with cache-manager-redis-store ^3.0.0
+      useFactory: async (config: ConfigService) => {
+        const store = await redisStore({
+          socket: {
+            host: config.get('REDIS_HOST'),
+            port: +config.get('REDIS_PORT'),
+          },
+        });
+        return {
+          store: store as unknown as CacheStore, // hacky, but no other option
+          ttl: 5 * 60 * 60, // 5h
+        };
+      },
+      isGlobal: true,
+    }),
+  ],
+  providers: [RedisService],
+})
+export class RedisModule {}
