7488557a1ddc454fd0ab95c3d6638cec65cf3e72
biplobmanna <biplobmanna@outlook.com>
2023-03-13 19:41:18 +0530

    feat: caching is now better
    - the caching functions are abstracted out, making code more consistent
    - instead of de-facto caching, it's more granular and specific
    - a middleware is added which performs a cache lookup for every incoming
    GET request on sensible routes
    - middleware adds two properties to request object
      - cacheKey : the key using which the cache is checked
      - params : the parsed query parameters
      - query params are required along with path for creating cache-key
      - so it makes sense to just add it to the request object, so as not to
        double compute the same thing
    - PUT, PATCH, DELETE requests clear the cache (good enough, but not very
      specific)
    - unnecessary Interceptors are removed from code

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,10 +1,49 @@
-import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common';
 import { Cache } from 'cache-manager';
 @Injectable()
 export class RedisService {
-  constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+  private readonly logger;
 
-  async clearCache() {
+  constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {
+    this.logger = new Logger(RedisService.name);
+  }
+
+  async reset() {
+    // this.logger.log('reset():');
     await this.cache.reset();
+  }
+
+  async get(key: string) {
+    // this.logger.log('get():' + key);
+    return await this.cache.get(key);
+  }
+
+  async set(key: string, val: any, ttl?: number) {
+    // this.logger.log('set():' + key);
+    // this.logger.log(val);
+    await this.cache.set(key, val, ttl);
+  }
+
+  async clear(key: string) {
+    // this.logger.log('clear():' + key);
+    await this.cache.del(key);
+  }
+
+  /**
+   *
+   * @param key string
+   * clears the cache based on the regex pattern (key + "*")
+   */
+  async clearAll(key?: string) {
+    // this.logger.log('clearAll():' + key);
+    if (!key || key.length == 0) {
+      return await this.cache.reset();
+    }
+
+    const allKeys: string[] = await this.cache.store.keys(key + '*');
+    for (const i in allKeys) {
+      // this.logger.log(`del: ${allKeys[i]}`);
+      await this.cache.del(allKeys[i]);
+    }
   }
 }
